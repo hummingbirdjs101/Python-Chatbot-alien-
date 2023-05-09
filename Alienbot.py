3d250102e13e0d3c256abdfa4e73faed4ea299b0
Valerie Raghoebier <valerie.raghoebier@outlook.com>
2023-05-08 18:53:04 -0700

    adding new python file

---
+++ Alienbot.py
@@ -0,0 +1,93 @@
+import re
+import random
+
+class AlienBot:
+  # potential negative responses
+  negative_responses = ("no", "nope", "nah", "naw", "not a chance", "sorry")
+  
+  # random starter questions
+  random_questions = (
+        "Why are you here? ",
+        "Are there many humans like you? ",
+        "What do you consume for sustenance? ",
+        "Is there intelligent life on this planet? ",
+        "Does Earth have a leader? ",
+        "What planets have you visited? ",
+        "What technology do you have on this planet? "
+    )
+
+  def __init__(self):
+    self.alienbabble = {'describe_planet_intent': r'.*\s*your planet.*',
+                        'answer_why_intent': r'why\sare\syou.*',
+                        'cubed_intent': r'.*cube.*(\d+)'
+                            }
+
+  # Define .greet() below:
+  def greet(self):
+    self.name = input("Welcome, what's your name?\n>")
+
+    will_help = input(f"Hi {self.name}, I'm Martian 2106. I'm not from this planet. Will you help me learn about your planet?\n>")
+
+    if will_help in self.negative_responses:
+      print("Ok, have a nice Earth day!")
+      return
+    self.chat()
+
+  # Define .make_exit() here:
+  def make_exit(self, reply):
+    exit_commands = ("quit", "pause", "exit", "goodbye", "bye", "later")
+    for exit_command in exit_commands:
+      if exit_command in reply:
+        print("Ok, have a nice Earth day!")
+        return True
+
+  # Define .chat() next:
+  def chat(self):
+    reply = input(random.choice(self.random_questions)).lower()
+    while not self.make_exit(reply):
+      input(self.match_reply(reply))
+
+  # Define .match_reply() below:
+  def match_reply(self, reply):
+    for key, value in (self.alienbabble).items():
+      intent = key
+      regex_pattern = value
+      found_match = re.match(regex_pattern, reply)     
+
+      if found_match and intent == 'describe_planet_intent':
+        self.describe_planet_intent()
+
+      elif found_match and intent == 'answer_why_intent':
+        self.answer_why_intent()
+    
+      #elif found_match and intent == 'cubed_intent':
+        #self.cubed_intent(found_match.group()[0])
+      else:
+        self.no_match_intent()
+
+
+  # Define .describe_planet_intent():
+  def describe_planet_intent(self):
+    responses = ("My planet is a utopia of diverse organisms and species. ", "I am from Opidipus, the capital of the Wayward Galaxies. ")
+    print(random.choice(responses))
+
+  # Define .answer_why_intent():
+  def answer_why_intent(self):
+    responses = ("I come in peace. ","I am here to collect data on your planet and its inhabitants. ","I heard the coffee is good. ")
+    print(random.choice(responses))
+       
+  # Define .cubed_intent():
+  def cubed_intent(self, number):
+    number = int(number)
+    cubed_number = number**3
+    return (f'The cube of {number} is {cubed_number}. Isnt that cool?')
+
+  # Define .no_match_intent():
+  def no_match_intent(self):
+    responses = ("Please tell me more.","Tell me more!","Why do you say that?","I see. Can you elaborate?","Interesting. Can you tell me more?","I see. How do you think?","Why? ","How do you think I feel when you say that? ")
+    print(random.choice(responses))
+
+# Create an instance of AlienBot below:
+my_bot = AlienBot()
+
+my_bot.greet()
